455667ab4b49104a9ac31d7422b7daefc0951ff2
GabrielSSahdo <gsahdo12@gmail.com>
2023-09-17 23:12:13 -0400

    Delete Node/Atividade 1/dir directory

--- Node/Atividade 1/dir/createLink.js
+++
@@ -1,8 +0,0 @@
-function createLink(dir, filename) {
-    return `<a href="/${dir}/${filename}">${filename}</a><br>\n`;
-    }
-function voltar(dir, filename){
-    return `<a href="/">Voltar</a>`  ;
-}
-
-module.exports = { createLink, voltar }
--- Node/Atividade 1/dir/thread/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs")
-
-fs.readFile("./../dir/arquivo.txt", "utf-8", (err, conteudo) => {
-    console.log(conteudo)
-})
-
-console.log("oiii")
